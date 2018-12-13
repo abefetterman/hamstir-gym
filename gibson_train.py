ebad255a79f96de8309275d1d5791ffec8d5e61c
Abe Fetterman <abe@nomiku.com>
2018-12-13 15:39:36 -0800

    use older baselines interface

--- gibson_train.py
+++ gibson_train.py
@@ -4,9 +4,11 @@ import baselines
 from baselines.ppo2 import ppo2
 from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
 from hamstir_gym.envs.hamstir_gibson_env import HamstirGibsonEnv
+from gibson.utils.fuse_policy2 import CnnPolicy2
 import os
+import argparse
 
-config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'configs', 'gibson_play.yaml')
+config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'configs', 'gibson_train_allensville.yaml')
 print(config_file)
         
 if __name__ == '__main__':
@@ -18,5 +20,5 @@ if __name__ == '__main__':
     env = HamstirGibsonEnv(config = args.config)
     env = DummyVecEnv([lambda: env])
     
-    ppo2.learn(network='cnn', env=env, total_timesteps=int(1e7), \
+    ppo2.learn(policy=CnnPolicy2, env=env, total_timesteps=int(1e7), \
                     log_interval = 1, save_interval = 10, gamma=0.95)
