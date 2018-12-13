63c87eb547e1617a82a37b68c50c3f0bd1dde7a9
Abe Fetterman <abe@nomiku.com>
2018-12-13 15:25:36 -0800

    fix gibson train config

--- gibson_train.py
+++ gibson_train.py
@@ -3,12 +3,20 @@ import hamstir_gym
 import baselines
 from baselines.ppo2 import ppo2
 from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
+from hamstir_gym.envs.hamstir_gibson_env import HamstirGibsonEnv
+import os
 
-def env_fn(): 
-    return gym.make('hamstir-gibson-v0')
+config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'configs', 'gibson_play.yaml')
+print(config_file)
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default=config_file)
+    args = parser.parse_args()
     
-env = DummyVecEnv([env_fn])
 
-ppo2.learn(network='cnn', env=env, total_timesteps=int(1e7), \
-                log_interval = 1, save_interval = 10, gamma=0.95) #, \
-                #load_path='./models/ppo2-cnn-160px-0500e.pt')
+    env = HamstirGibsonEnv(config = args.config)
+    env = DummyVecEnv([lambda: env])
+    
+    ppo2.learn(network='cnn', env=env, total_timesteps=int(1e7), \
+                    log_interval = 1, save_interval = 10, gamma=0.95)
