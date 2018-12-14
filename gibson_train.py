b057683c86dd49286dd80b0f8a98294228767e33
Abe Fetterman <abe@nomiku.com>
2018-12-13 16:10:23 -0800

    back to stable_baselines

--- gibson_train.py
+++ gibson_train.py
@@ -1,107 +1,59 @@
-#add parent dir to find package. Only needed for source code build, pip install doesn't need it.
-import os, inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(os.path.dirname(currentdir))
-os.sys.path.insert(0,parentdir)
-
-import gym, logging
-from mpi4py import MPI
 from hamstir_gym.envs.hamstir_gibson_env import HamstirGibsonEnv
-from baselines.common import set_global_seeds
-import baselines.common.tf_util as U
-from gibson.utils.fuse_policy2 import MlpPolicy, MlpPolicy2, CnnPolicy2
-from baselines.common.atari_wrappers import make_atari, wrap_deepmind
-from gibson.utils import utils
-import datetime
-from baselines import logger
-#from baselines.ppo2 import ppo2
-from gibson.utils import ppo2
-from gibson.utils import ppo2_imgs
-from gibson.utils.monitor import Monitor
-import os.path as osp
-import tensorflow as tf
-import random
-import sys
+import argparse
+import os
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
 
-## Training code adapted from: https://github.com/openai/baselines/blob/master/baselines/ppo1/run_atari.py
+from stable_baselines.common.policies import CnnPolicy
+from stable_baselines.common.vec_env.dummy_vec_env import DummyVecEnv
+from stable_baselines.bench import Monitor
+from stable_baselines.results_plotter import load_results, ts2xy
+from stable_baselines import PPO2
 
-def train(num_timesteps, seed):
-    rank = MPI.COMM_WORLD.Get_rank()
-    #sess = U.single_threaded_session()
-    sess = utils.make_gpu_session(args.num_gpu)
-    sess.__enter__()
-    if args.meta != "":
-        saver = tf.train.import_meta_graph(args.meta)
-        saver.restore(sess,tf.train.latest_checkpoint('./'))
+config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'configs', 'gibson_train_allensville.yaml')
+print(config_file)
 
-    if rank == 0:
-        logger.configure()
-    else:
-        logger.configure(format_strs=[])
-    workerseed = seed + 10000 * MPI.COMM_WORLD.Get_rank()
-    set_global_seeds(workerseed)
+best_mean_reward, n_steps = -np.inf, 0
 
-    use_filler = not args.disable_filler
+def callback(_locals, _globals):
+  """
+  Callback called at each step (for DQN an others) or after n steps (see ACER or PPO2)
+  :param _locals: (dict)
+  :param _globals: (dict)
+  """
+  global n_steps, best_mean_reward
+  # Evaluate policy performance
+  x, y = ts2xy(load_results(log_dir), 'timesteps')
+  if len(x) > 0:
+      mean_reward = np.mean(y[-100:])
+      print(x[-1], 'timesteps')
+      print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(best_mean_reward, mean_reward))
 
-    config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'configs', 'gibson_train_allensville.yaml')
-    print(config_file)
-
-    raw_env = HamstirGibsonEnv(gpu_idx=args.gpu_idx,
-                                config=config_file)
-
-    env = Monitor(raw_env, logger.get_dir() and
-        osp.join(logger.get_dir(), str(rank)))
-    env.seed(workerseed)
-
-    gym.logger.setLevel(logging.WARN)
-
-    ppo2.learn(policy=CnnPolicy2, env=env, nsteps=3000, nminibatches=4,
-        lam=0.95, gamma=0.99, noptepochs=4, log_interval=1,
-        ent_coef=.1,
-        lr=lambda f : f * 2e-4,
-        cliprange=lambda f : f * 0.2,
-        total_timesteps=int(num_timesteps * 1.1),
-        save_interval=5,
-        sensor= args.mode == "SENSOR",
-        reload_name=args.reload_name)
-    
-    '''
-    pposgd_fuse.learn(env, policy_fn,
-        max_timesteps=int(num_timesteps * 1.1),
-        timesteps_per_actorbatch=1024,
-        clip_param=0.2, entcoeff=0.0001,
-        optim_epochs=10, optim_stepsize=3e-6, optim_batchsize=64,
-        gamma=0.995, lam=0.95,
-        schedule='linear',
-        save_name=args.save_name,
-        save_per_acts=10000,
-        reload_name=args.reload_name
-    )
-
-    env.close()
-    '''
-
-def callback(lcl, glb):
-    # stop training if reward exceeds 199
-    total = sum(lcl['episode_rewards'][-101:-1]) / 100
-    totalt = lcl['t']
-    is_solved = totalt > 2000 and total >= -50
-    return is_solved
+      # New best model, you could save the agent here
+      if mean_reward > best_mean_reward:
+          best_mean_reward = mean_reward
+          # Example for saving best model
+          print("Saving new best model")
+          _locals['self'].save(log_dir + 'best_model.pkl')
+  return True
 
 
-def main():
-    train(num_timesteps=10000000, seed=5)
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--mode', type=str, default="RGB")
-    parser.add_argument('--num_gpu', type=int, default=1)
-    parser.add_argument('--gpu_idx', type=int, default=0)
-    parser.add_argument('--disable_filler', action='store_true', default=False)
-    parser.add_argument('--meta', type=str, default="")
-    parser.add_argument('--resolution', type=str, default="SMALL")
-    parser.add_argument('--reload_name', type=str, default=None)
-    parser.add_argument('--save_name', type=str, default=None)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default=config_file)
     args = parser.parse_args()
-    main()
+    
+    # Create log dir
+    log_dir = "/tmp/gym/"
+    os.makedirs(log_dir, exist_ok=True)
+
+    env = HamstirGibsonEnv(config=args.config)
+    env = Monitor(env, log_dir, allow_early_resets=True)
+    env = DummyVecEnv([lambda: env])
+    
+    model = PPO2(CnnPolicy, env, verbose=1)
+    
+    # print(env.config)
+    model.learn(total_timesteps=100000, callback=callback)
