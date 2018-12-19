05ba3384343d18456b004d8357a9d305dd9ea4e5
Abe Fetterman <abe@nomiku.com>
2018-12-19 09:25:42 -0800

    use texture.py for testing

--- hamstir_gym/modder.py
+++ hamstir_gym/modder.py
@@ -5,7 +5,7 @@ from gym.utils import seeding
 from hamstir_gym.utils import DATA_DIR
 
 class Modder:
-    def __init__(self, h=32, w=32):
+    def __init__(self, h=256, w=256):
         self.h,self.w = h, w
         self.pixels = np.zeros((h,w,3),dtype=np.int32)
         self.seed()
@@ -21,7 +21,7 @@ class Modder:
         self.textures = []
         for j in self.joints:
             p.changeVisualShape(parent,j,rgbaColor=[1,1,1,1])
-            t = p.loadTexture(DATA_DIR+"tex32.png")
+            t = p.loadTexture(DATA_DIR+"tex256.png")
             self.textures.append(t)
             p.changeTexture(t,self.random_pixels(),self.w,self.h)
             p.changeVisualShape(parent,j,textureUniqueId=t)
--- texture.py
+++ texture.py
@@ -2,18 +2,25 @@ import pybullet as p
 import time
 import pybullet_data
 from hamstir_gym.modder import Modder
+from tqdm import tqdm
 
 mod = Modder()
-physicsClient = p.connect(p.GUI)
+physicsClient = p.connect(p.DIRECT)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 # planeId = p.loadURDF("plane.urdf")
-room=p.loadURDF("./hamstir_gym/data/room12x12mesh.urdf")
+room=p.loadURDF("./hamstir_gym/data/room12x12.urdf")
 mod.load(room)
 # mod.randomize()
 # 
+pbar = tqdm()
+frames = 0
 try:
     while True:
         p.stepSimulation()
-        time.sleep(1/20.)
+        p.getCameraImage(128,128,renderer=p.ER_BULLET_HARDWARE_OPENGL)
+        mod.randomize()
+        # time.sleep(1/20.)
+        frames += 1
+        pbar.update(frames) 
 except:
     print('bye')
